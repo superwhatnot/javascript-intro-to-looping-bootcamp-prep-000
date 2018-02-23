ad289ad867d1464837613299ec4b7d841cc2ed52
Raja H <rajahamid12.7@gmail.com>
2018-02-23 01:22:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,7 +12,13 @@ return the array.
 
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
-    array.push(`I am a strange loop.`)
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`)
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
   return array
 }
+    
